9913ffcb5e533aafc8f7ce67c47a8cd01f1f9db0
johnny <johnny@localhost.local>
2022-11-28 14:45:16 -0800

    legacy provider knowledge

--- remotestate/main.tf
+++ remotestate/main.tf
@@ -10,9 +10,7 @@ resource "aws_s3_bucket" "s3_bucket" {
 
 resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
     bucket = aws_s3_bucket.s3_bucket.id
-    versioning_configuration {
-        enabled = true
-    }
+    enabled = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_sse" {
