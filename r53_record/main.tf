33e20b9857d631b48486209446fc6435f4dac891
johnny <johnny@localhost.lan>
2022-11-29 11:52:23 -0800

    updated each value

--- r53_record/main.tf
+++ r53_record/main.tf
@@ -2,8 +2,8 @@ resource "aws_route53_record" "record" {
   for_each = toset(var.record_map)
 
   zone_id = each.value.zone_id
-  name    = each.value.dns_name
-  type    = each.value.record_type
+  name    = each.value.name
+  type    = each.value.type
   records = each.value.records
 
   ttl = var.record_ttl
