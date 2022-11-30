f0cc2c024441f4aba4841f2a75d99dc0275a6448
johnny <johnny@localhost.local>
2022-11-29 16:55:59 -0800

    updating function code

--- s3_static_site/lambda/index.js
+++ s3_static_site/lambda/index.js
@@ -1,22 +1,15 @@
-'use strict';
+function handler(event) {
+  var request = event.request;
+  var uri = request.uri;
+  
+  // Check whether the URI is missing a file name.
+  if (uri.endsWith('/')) {
+      request.uri += 'index.html';
+  } 
+  // Check whether the URI is missing a file extension.
+  else if (!uri.includes('.')) {
+      request.uri += '/index.html';
+  }
 
-exports.handler = (event, context, callback) => {
-    // Extract the request from the CloudFront event that is sent to Lambda@Edge 
-    var request = event.Records[0].cf.request;
-
-    // Extract the URI from the request
-    var olduri = request.uri;
-
-    // Match any '/' that occurs at the end of a URI. Replace it with a default index
-    var newuri = olduri.replace(/\/$/, '\/index.html');
-    
-    // Log the URI as received by CloudFront and the new URI to be used to fetch from origin
-    console.log("Old URI: " + olduri);
-    console.log("New URI: " + newuri);
-    
-    // Replace the received URI with the URI that includes the index page
-    request.uri = newuri;
-    
-    // Return to CloudFront
-    return callback(null, request);
-};
+  return request;
+}
