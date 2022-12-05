00805f962f58346210a16fb95917809624071dea
jonathanwalker <{ID}+{username}@users.noreply.github.com>
2022-12-04 22:39:01 -0800

    adding security headers policy

--- s3_static_site/main.tf
+++ s3_static_site/main.tf
@@ -11,7 +11,8 @@ resource "aws_cloudfront_distribution" "distribution" {
   enabled         = true
   is_ipv6_enabled = true
 
-  default_root_object = var.default_root_object
+  default_root_object        = var.default_root_object
+  response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers.id
 
   origin {
     domain_name = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
