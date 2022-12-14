05a97af51554a2af01219543421302eb8675cd07
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2022-12-14 12:51:18 -0800

    this cannot work

--- s3_fim_notification/main.tf
+++ s3_fim_notification/main.tf
@@ -1,7 +1,7 @@
 # s3_bucket_notification to lambda function
 resource "aws_s3_bucket_notification" "s3_bucket_notification" {
   depends_on = [aws_lambda_function.function]
-  
+
   bucket = var.bucket_name
 
   lambda_function {
@@ -10,4 +10,15 @@ resource "aws_s3_bucket_notification" "s3_bucket_notification" {
     filter_prefix       = var.filter_prefix
     filter_suffix       = var.filter_suffix
   }
+}
+
+# permission to invoke lambda from bucket notification
+resource "aws_lambda_permission" "s3_bucket_notification" {
+  depends_on = [aws_s3_bucket_notification.s3_bucket_notification]
+
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.function.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.bucket_name}"
 }
