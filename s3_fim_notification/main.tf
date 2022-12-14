1d12d7f8a4b9645beed8626d44e09558edfa75f3
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2022-12-14 15:18:26 -0800

    changed up main

--- s3_fim_notification/main.tf
+++ s3_fim_notification/main.tf
@@ -4,32 +4,51 @@ resource "aws_s3_bucket_notification" "s3_bucket_notification" {
 
   bucket = var.bucket_name
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.function.arn
-    events              = var.s3_events
-    filter_prefix       = var.filter_prefix
-    filter_suffix       = var.filter_suffix
+  queue {
+    queue_arn = aws_sqs_queue.queue.arn
+    events    = var.s3_events
+
+    filter_prefix = var.filter_prefix
+    filter_suffix = var.suffix_filter
   }
 }
 
-resource "null_resource" "build" {
-  # trigger when src/main.go changes
-  triggers = {
-    main = filemd5("${path.module}/src/main.go")
-  }
-  
-  provisioner "local-exec" {
-    command = "cd ${path.module}/src && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o main"
+# create sqs queue for s3_bucket_notification
+resource "aws_sqs_queue" "queue" {
+  name = "${var.bucket_name}-fim-queue"
+}
+
+# create sqs queue policy for s3_bucket_notification
+resource "aws_sqs_queue_policy" "queue_policy" {
+  queue_url = aws_sqs_queue.queue.id
+
+  policy = data.aws_iam_policy_document.queue_policy.json
+}
+
+data "aws_iam_policy_document" "queue_policy" {
+  statement {
+    sid = "sqs_policy"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+    actions = ["SQS:SendMessage"]
+    resources = [aws_sqs_queue.sqs_queue.arn]
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values = [aws_s3_bucket_notification.s3_bucket_notification.arn]
+    }
   }
 }
 
-# permission to invoke lambda from bucket notification
-resource "aws_lambda_permission" "s3_bucket_notification" {
-  depends_on = [aws_s3_bucket_notification.s3_bucket_notification]
+# sqs queue to trigger lambda function
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  depends_on = [aws_sqs_queue_policy.queue_policy]
 
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.function.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::${var.bucket_name}"
+  event_source_arn = aws_sqs_queue.queue.arn
+  function_name    = aws_lambda_function.function.arn
+  batch_size       = var.batch_size
+  enabled          = true
 }
