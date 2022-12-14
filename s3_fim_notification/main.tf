89c13a9255648f30f53c84ec715c846c1860d177
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2022-12-14 12:31:59 -0800

    left notes

--- s3_fim_notification/main.tf
+++ s3_fim_notification/main.tf
@@ -9,13 +9,3 @@ resource "aws_s3_bucket_notification" "s3_bucket_notification" {
     filter_suffix       = var.filter_suffix
   }
 }
-
-
-resource "aws_s3_bucket_notification" "s3_bucket_notification" {
-  bucket = var.bucket_name
-
-  topic {
-    topic_arn = aws_sns_topic.sns_topic.arn
-    events    = var.s3_events
-  }
-}
