710230890a7661fa36ef1c9bb0ef5a57f366eb43
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2022-12-14 13:01:16 -0800

    updating go

--- s3_fim_notification/src/main.go
+++ s3_fim_notification/src/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 // lambda handler to print the event
 func Handler(event interface{}) (string, error) {
 	fmt.Println(event)
 	return "OK", nil
+}
+
+func main() {
+	lambda.Start(Handler)
 }
