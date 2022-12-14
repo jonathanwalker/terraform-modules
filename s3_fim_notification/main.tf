6e011896671f7678083cb5affe6f234de7e16ab4
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2022-12-14 14:28:29 -0800

    updated trigger

--- s3_fim_notification/main.tf
+++ s3_fim_notification/main.tf
@@ -13,6 +13,11 @@ resource "aws_s3_bucket_notification" "s3_bucket_notification" {
 }
 
 resource "null_resource" "build" {
+  # trigger when src/main.go changes
+  triggers = {
+    main = filemd5("${path.module}/src/main.go")
+  }
+  
   provisioner "local-exec" {
     command = "cd ${path.module}/src && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o main"
   }
