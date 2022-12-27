fc8e96a17adfd32d5614360623d93a9ca8536a70
Jonathan Walker <14978093+jonathanwalker@users.noreply.github.com>
2022-12-27 09:34:10 -0800

    you should really sanitize user inputs

--- s3_static_site/lambda/index.js
+++ s3_static_site/lambda/index.js
@@ -3,6 +3,10 @@
 exports.handler = async function(event) {
   var request = event.Records[0].cf.request;
   var uri = request.uri;
+  
+  // Sanitize user input for any dissallowed characters
+  // Lowercase letters (a-z), uppercase letters (A-Z), numbers (0-9), period (.), forward slash (/), and hyphens (-) allowed
+  uri = uri.replace(/[^a-zA-Z0-9\.\/-]/g, '');
     
   // Check whether the URI is missing a file name.
   if (uri.endsWith('/')) {
