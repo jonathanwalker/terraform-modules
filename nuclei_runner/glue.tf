b7f7f191b28433314782f196fecf9de6937c21ac
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2022-12-31 08:59:47 -0800

    reducing projection

--- nuclei_runner/glue.tf
+++ nuclei_runner/glue.tf
@@ -99,7 +99,7 @@ resource "aws_glue_catalog_table" "table" {
     "projection.dt.format"        = "yyyy/MM/dd/HH"
     "projection.dt.interval"      = "1"
     "projection.dt.interval.unit" = "HOURS"
-    "projection.dt.range"         = "NOW-3YEARS,NOW"
+    "projection.dt.range"         = "NOW-1YEARS,NOW"
     "storage.location.template"   = "s3://${aws_s3_bucket.bucket.id}/findings/$${dt}"
   }
 }
