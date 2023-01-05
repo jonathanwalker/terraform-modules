4bffc0e9f30353a18ce899b8554a198938b3910a
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-01-05 12:44:12 -0800

    updating code

--- nuclei_api/src/main.go
+++ nuclei_api/src/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 type Event struct {
 	Name string `json:"name"`
 }
 
-func handler(ctx context.Context, event Event) {
+func handler(ctx context.Context, event Event) (interface{}, error) {
 	// Print the name
 	fmt.Println("Name:", event.Name)
 
@@ -19,12 +22,41 @@ func handler(ctx context.Context, event Event) {
 	} else {
 		fmt.Println("Executed via: Direct Lambda Invocation")
 	}
+
+	// Return a response to API Gateway
+	return map[string]string{
+		"message": fmt.Sprintf("Hello %s", event.Name),
+	}, nil
 }
 
 func main() {
-	// Create an event
-	event := Event{Name: "Bob"}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Parse the request body
+		var event Event
+		err := json.NewDecoder(r.Body).Decode(&event)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	// Invoke the handler with a nil context
-	handler(nil, event)
+		// Invoke the handler with a nil context
+		res, err := handler(nil, event)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Encode the response as JSON
+		resData, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the Content-Type header and write the response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(resData)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
