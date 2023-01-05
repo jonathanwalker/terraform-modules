51e64b3bf05e92addc8f7177ae71ed85962ca00a
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-01-05 15:49:21 -0800

    comment

--- nuclei_api/main.tf
+++ nuclei_api/main.tf
@@ -45,9 +45,7 @@ resource "aws_lambda_permission" "permission" {
   function_name = aws_lambda_function.function.arn
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
-
-  # source_arn = aws_api_gateway_deployment.deployment.execution_arn
+  #source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 # Layer to run nuclei in lambda
