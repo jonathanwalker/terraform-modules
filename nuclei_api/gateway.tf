5be6364f88f45ab2bf4fc0c6e99f3fe0c445cb55
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-01-05 11:47:53 -0800

    api gateway

--- nuclei_api/gateway.tf
+++ nuclei_api/gateway.tf
@@ -35,13 +35,51 @@ resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.integration]
 }
 
+resource "aws_api_gateway_domain_name" "domain" {
+  domain_name              = "api.example.com"
+  regional_certificate_arn = aws_acm_certificate_validation.example.certificate_arn
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  tags = var.tags
+}
+
+resource "aws_route53_record" "record_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.record_validation : record.fqdn]
+}
+
 resource "aws_route53_record" "record" {
   name = var.domain
   type = "A"
   zone_id = var.zone_id
   alias {
-    name = aws_api_gateway_rest_api.gateway.execution_arn
-    zone_id = aws_api_gateway_rest_api.gateway.execution_arn
+    name = aws_api_gateway_domain_name.domain.regional_domain_name
+    zone_id = aws_api_gateway_domain_name.domain.regional_zone_id
     evaluate_target_health = false
   }
 }
