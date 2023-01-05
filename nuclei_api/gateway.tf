3857873e829f4fe0f3a1b8c92a86bc109a40eb39
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-01-05 15:44:15 -0800

    perhaps invocation arn

--- nuclei_api/gateway.tf
+++ nuclei_api/gateway.tf
@@ -13,7 +13,7 @@ resource "aws_api_gateway_resource" "resource" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id = aws_api_gateway_rest_api.gateway.id
   resource_id = aws_api_gateway_resource.resource.id
-  http_method = "ANY"
+  http_method = "GET"
   authorization = "NONE"
 }
 
@@ -24,7 +24,7 @@ resource "aws_api_gateway_integration" "integration" {
 
   type = "AWS_PROXY"
   integration_http_method = "POST"
-  uri = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.function.arn}/invocations"
+  uri = aws_lambda_function.function.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
