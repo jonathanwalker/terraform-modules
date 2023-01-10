1c74e2bbf87dd620ff0f2f50317924b9846d0ca6
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-01-09 20:21:14 -0800

    outputs and layers

--- nuclei_runner/output.tf
+++ nuclei_runner/output.tf
@@ -1,7 +1,7 @@
 output "function_name" { 
-  value = aws_lambda_function.lambda_function.name 
+  value = aws_lambda_function.function.name 
 }
 
 output "function_region" { 
-  value = aws_lambda_function.lambda_function.region 
+  value = aws_lambda_function.function.region 
 }
