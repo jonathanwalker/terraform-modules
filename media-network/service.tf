5a5fb1205aeaf11957e1bbdadc74eef75f1ade41
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-02-12 16:51:09 -0800

    all egress

--- media-network/service.tf
+++ media-network/service.tf
@@ -59,9 +59,9 @@ resource "aws_security_group_rule" "jellyfin_ingress_efs" {
 
 resource "aws_security_group_rule" "jellyfin_egress" {
   type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.jellyfin.id
 }
