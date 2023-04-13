a511a6af3a0b0b9919a4db3201fe56c32e57cf3a
jonathanwalker <14978093+jonathanwalker@users.noreply.github.com>
2023-04-12 23:23:01 -0700

    required providers

--- unifi/networks/providers.tf
+++ unifi/networks/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    unifi = {
+      source = "paultyng/unifi"
+      version = "0.41.0"
+    }
+  }
+}
+
 provider "unifi" {
   username = "${env.UNIFI_USERNAME}"
   password = "${env.UNIFI_PASSWORD}"
